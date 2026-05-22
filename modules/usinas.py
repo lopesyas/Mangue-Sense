d10d3f69dd081f9b5a01caa7f5ad20d0d896307a
S-Bione <sbab@cesar.school>
2026-05-22 01:40:22 -0300

    fiz a base do cadastro

--- modules/usinas.py
+++ modules/usinas.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 usinas = [
     {
         "id": 1,
@@ -46,3 +47,58 @@ if resposta.upper() == "S":
 
 else:
     print("Exclusão cancelada.")
+=======
+import json
+import os
+
+def cadastrar_usina():
+    salvar_usinas = {}
+
+    salvar_usinas["ID da usina"] =  int(input("ID da usina: "))
+
+    salvar_usinas["Nome da usina"] = input("Nome da usina: ").capitalize()
+    
+    salvar_usinas["Empresa responsável pela usina"] = input("Empresa responsável pela usina: ").capitalize()
+
+    salvar_usinas["Cidade da usina"] = input("Cidade da usina: ").capitalize()
+
+    salvar_usinas["UF da usina"] = input("UF da usina: ").upper().strip()
+
+    salvar_usinas["Potência da usina (kWp)"] = float(input("Potência da usina (kWp): "))
+
+    salvar_usinas["Quantidade de painéis"] = int(input("Quantidade de painéis: "))
+
+    salvar_usinas["Data de instalação"] = input("Data de instalação: ").strip()
+  
+    salvar_usinas["Status da usina"] = input("Status da usina: ").upper()   
+
+    return salvar_usinas 
+
+def salvar_usinas_json(dados):
+
+    caminho = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "data",
+        "usinas.json"
+    )
+
+    with open(caminho, "w", encoding="utf-8") as arquivo:
+        json.dump(dados, arquivo, indent = 4, ensure_ascii=False)
+
+usina = cadastrar_usina()
+salvar_usinas_json(usina)
+
+def listar_usinas():
+    print("=========== LISTA DE USINAS =========== ")
+    print(usina["ID da usina"])
+    print(usina["Nome da usina"])
+    print(usina["Empresa responsável pela usina"])
+    print(usina["Cidade da usina"])
+    print(usina["UF da usina"])
+    print(f"{usina["Potência da usina (kWp)"]}")
+    print(usina["Quantidade de painéis"])
+    print(usina["Data de instalação"])
+    print(usina["Status da usina"])
+    print("=======================================")
+>>>>>>> 298c3e9 (fiz a base do cadastro (tentando dnv, pq antes n foi))
